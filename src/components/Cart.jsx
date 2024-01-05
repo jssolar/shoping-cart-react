54a3284999e35ee0179cdc7d2e39aa942d4abf98
Juan Solar <juansolarsolar@yahoo.com>
2024-01-05 01:33:26 -0300

    terminado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,33 +3,53 @@ import {  CartIcon, ClearCartIcon,  } from "./Icons";
 import './Cart.css'
 import { useCart } from "../hooks/useCart";
 
+
+
+function CartItem({thumbnail, price, title, quantity, addToCart}){
+
+  return(
+    <li>
+    <img src={thumbnail} alt={title} />
+    <div>
+      <strong>{title}</strong> - ${price}
+    </div>
+    <footer>
+      <small>
+        Qty: {quantity}
+      </small>
+      <button onClick={addToCart}>+</button>
+    </footer>
+  </li>
+  )
+}
+
+
+
+
+
 export function Cart() {
   const cartCheckBoxId = useId()
-  const { cart, clearCart} = useCart()
+  const { cart, clearCart, addToCart} = useCart()
 
 
 
 
   return(
     <>
-      <label htmlFor={cartCheckBoxId} className="cart-button">
+      <label className="cart-button" htmlFor={cartCheckBoxId} >
         <CartIcon />
       </label>
-        <input type="checkbox" hidden id={cartCheckBoxId} />
+        <input  id={cartCheckBoxId} type="checkbox" hidden />
         <aside className="cart">
           <ul>
-            <li>
-              <img src="https://i.dummyjson.com/data/products/2/thumbnail.jpg" alt="Iphone" />
-              <div>
-                <strong>iPhone</strong> - $1499
-              </div>
-              <footer>
-                <small>
-                  Qty: 1
-                </small>
-                <button>+</button>
-              </footer>
-            </li>
+            {cart.map(product => (
+                <CartItem
+                   key={product.id}
+                   addToCart={()=>{addToCart(product)}}
+                   {...product}
+                />
+              ))
+            }
           </ul>
           <button onClick={clearCart}>
             <ClearCartIcon />
--- src/conext/cart.jsx
+++ src/conext/cart.jsx
@@ -1,52 +1,99 @@
-import { createContext, useState } from "react";
-// 1. Crear contexto
+import { useReducer, createContext } from 'react'
+import { cartReducer, cartInitialState } from '../reducers/cart.js'
+
 export const CartContext = createContext()
 
-// 2. Crear provider
-export function CartProvider({ children }) {
-  const [cart, setCart] = useState([])
+function useCartReducer () {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
 
-  const addToCart = (product) => {
-    /* setCart([ ...cart, product])//esta es la forma más sensilla de añadir algo al carro */
-    // Check if the product is already in the cart
-    
-    const productInTheCartIndex = cart.findIndex(item => item.id === product.id)
-    
-    if(productInTheCartIndex >= 0 ) {
-      //una forma usando structuredClone
-      const newCart = structuredClone(cart)
-      newCart[productInTheCartIndex].quantity += 1
-       return setCart(newCart)
-    }
-    // Si product no está en cart
-    setCart(prevState => ([
-      ...prevState, 
-      {
-        ...product,
-        quantity: 1
-      }
-    ]))
-   }
+  const addToCart = product => dispatch({
+    type: 'ADD_TO_CART',
+    payload: product
+  })
 
+  const removeFromCart = product => dispatch({
+    type: 'REMOVE_FROM_CART',
+    payload: product
+  })
 
-   const removeFromCart = (product) =>{
-    setCart(prevState => prevState.filter(item => item.id !== product.id))
-   }
+  const clearCart = () => dispatch({ type: 'CLEAR_CART' })
 
+  return {  addToCart, state, removeFromCart, clearCart }
+}
 
-
-  const clearCart = () => {
-    setCart([])
-  }
+// la dependencia de usar React Context
+// es MÍNIMA
+export function CartProvider ({ children }) {
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
 
   return (
     <CartContext.Provider value={{
-      removeFromCart,
-      cart,
+      cart: state,
       addToCart,
+      removeFromCart,
       clearCart
-    }}>
+    }}
+    >
       {children}
     </CartContext.Provider>
   )
 }
+
+
+
+
+
+// import { createContext, useState } from "react";
+// // 1. Crear contexto
+// export const CartContext = createContext()
+
+// // 2. Crear provider
+// export function CartProvider({ children }) {
+//   const [cart, setCart] = useState([])
+
+//   const addToCart = (product) => {
+//     /* setCart([ ...cart, product])//esta es la forma más sensilla de añadir algo al carro */
+//     // Check if the product is already in the cart
+    
+//     const productInTheCartIndex = cart.findIndex(item => item.id === product.id)
+    
+//     if(productInTheCartIndex >= 0 ) {
+//       //una forma usando structuredClone
+//       const newCart = structuredClone(cart)
+//       newCart[productInTheCartIndex].quantity += 1
+//        return setCart(newCart)
+//     }
+//     // Si product no está en cart
+//     setCart(prevState => ([
+//       ...prevState, 
+//       {
+//         ...product,
+//         quantity: 1
+//       }
+//     ]))
+//    }
+
+
+//    const removeFromCart = (product) =>{
+//     setCart(prevState => prevState.filter(item => item.id !== product.id))
+//    }
+
+
+
+//   const clearCart = () => {
+//     setCart([])
+//   }
+
+//   return (
+//     <CartContext.Provider value={{
+//       removeFromCart,
+//       cart,
+//       addToCart,
+//       clearCart
+//     }}>
+//       {children}
+//     </CartContext.Provider>
+//   )
+// }
+
+
